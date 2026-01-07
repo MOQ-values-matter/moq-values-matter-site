769da7b1a4b470e6e861b7baf86541c1eb8310a7
Ulvefarfar <krs@live.no>
2026-01-07 21:02:28 +0100

    Updated config
    To remove the standard guides

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'MOQ Values Matter',
-			social: [
-				{
-					icon: 'github',
-					label: 'GitHub',
-					href: 'https://github.com/moq-values-matter/moq-values-matter-site',
-				},
-			],
-			sidebar: [
-				{
-					label: 'Fundamentals',
-					autogenerate: { directory: 'fundamentals' },
-				},
-				{
-					label: 'Value Atlas',
-					autogenerate: { directory: 'atlas' },
-				},
-				{
-					label: 'Essays',
-					autogenerate: { directory: 'essays' },
-				},
-			],
-		}),
-	],
+  site: "https://moq-values-matter.github.io",
+  base: "/moq-values-matter-site",
+  integrations: [
+    starlight({
+      title: "MOQ Values Matter",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/MOQ-values-matter/moq-values-matter-site",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Fundamentals",
+          autogenerate: { directory: "fundamentals" },
+        },
+        {
+          label: "Value Atlas",
+          autogenerate: { directory: "atlas" },
+        },
+        {
+          label: "Essays",
+          autogenerate: { directory: "essays" },
+        },
+      ],
+    }),
+  ],
 });
